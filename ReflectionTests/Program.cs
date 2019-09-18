4b1cb728f0dc04870d5c7ad0bf8a4e5045d828fc
Eser Kuru <eser.kuru@infoline-tr.com>
2019-09-18 14:44:16 +0300

    Initial commit

---
+++ ReflectionTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClassBuilder MCB = new MyClassBuilder("Student");
+            var myclass = MCB.CreateObject(new string[3] { "ID", "Name", "Address" }, new Type[3] { typeof(int), typeof(string), typeof(string) });
+            Type TP = myclass.GetType();
+
+            foreach (PropertyInfo PI in TP.GetProperties())
+            {
+                Console.WriteLine(PI.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
